8c307bf7aaa177ae7ce725b0e477fba2af35a57a
Far-sa <fmohtasham82@gmail.com>
2022-09-21 16:27:04 +0300

    Use node/python command to read files

--- execFile.js
+++ execFile.js
@@ -1,13 +1,13 @@
 const { execFile } = require('child_process')
 const { exec } = require('child_process')
 
-exec('chmod +x index.py', (err, stdout, stderr) => {
+exec('chmod +x exec.js', (err, stdout, stderr) => {
   if (err) return console.log('Error', err)
   if (stderr) return console.log('stdError', stderr)
   console.log('stdout', stdout)
 })
 
-execFile('./index.py', (err, stdout, stderr) => {
+execFile('./exec.js', (err, stdout, stderr) => {
   if (err) return console.log('Error', err.message)
   if (stderr) return console.log('stderr', err.message)
   console.log('stdout', stdout)
