74de46ebf90bce50c29b5b62131fd4ce938bc824
Far-sa <fmohtasham82@gmail.com>
2022-09-21 17:02:19 +0300

    modify spawn methods of using childs thread

--- spawn.js
+++ spawn.js
@@ -1,16 +1,28 @@
 const { spawn } = require('child_process')
 
-const child = spawn('ls', ['-lh'])
+// const child = spawn('ls', ['-lh'])
 
-child.stdout.on('data', data => {
-  console.log('stdout :', data.toString())
-})
+// child.stdout.on('data', data => {
+//   console.log('stdout :', data.toString())
+// })
 
-child.stderr.on('error', error => {
-  console.log('stderr', error.toString())
-})
+// child.stderr.on('error', error => {
+//   console.log('stderr', error.toString())
+// })
 
-child.on('exit', (code, signal) => {
-  console.log('process exit with code :', code)
-  console.log('process has been killed with :', signal)
-})
+// child.on('exit', (code, signal) => {
+//   console.log('process exit with code :', code)
+//   console.log('process has been killed with :', signal)
+// })
+
+for (let index = 0; index < 5; index++) {
+  const child = spawn('node', ['help.js', process.pid])
+
+  child.stdout.on('data', data => {
+    console.log('stdout :', data.toString())
+  })
+
+  child.stderr.on('error', error => {
+    console.log('stderr', error.toString())
+  })
+}
