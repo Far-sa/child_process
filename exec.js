149ae4da710d29c8d55707bdb25679ef663b2611
Far-sa <fmohtasham82@gmail.com>
2022-09-21 14:59:59 +0300

    modify exec.js

--- exec.js
+++ exec.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process')
 const { stdout, stderr } = require('process')
 
-exec('cd exec&&touch index.txt', (err, stdout, stderr) => {
+exec('ls -lh&&pwd&&touch index.html', (err, stdout, stderr) => {
   if (err) return console.log('Error', err)
   if (stderr) return console.log('stdError', stderr)
   console.log('stdout', stdout)
